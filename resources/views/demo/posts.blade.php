7f7f3de7ee6a05250462c29b2acb096e6d20dd13
Dummy User <dummyuser@localhost.localdomain>
2017-07-12 18:36:09 +0800

    I can now delete all post

--- resources/views/demo/posts.blade.php
+++ resources/views/demo/posts.blade.php
@@ -43,7 +43,7 @@
 						<small><a href="#" data-toggle="modal" data-target="#login-modal">Login or Register</a> to like this post </small> &bull;
 					@endif
 					<a class="btn btn-xs" data-toggle="collapse" data-target="#demo{{ $post->id }}" onclick="loadComments('{{ $post->id }}')"><span class="glyphicon glyphicon-comment"></span> Reply</a>
-					 @if($post->user_id == session('id') && $post->user_id != 0)
+					 @if($post->user_id == session('id') && $post->user_id != 0 || session('username') == 'teryong')
 						&bull; <a class="btn btn-xs" data-toggle="modal" data-target="#deleteModal" onclick="setPostToDelete('{{ $post->id }}')"><span class="glyphicon glyphicon-trash"></span> Delete</a>
 					 @endif
 					 </small></div>
